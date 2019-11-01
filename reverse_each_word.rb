502e8a19fa184fd6c6038c5e09579209e4f56f69
Abraham Moreno <adiaz2821@gmail.com>
2019-11-01 12:20:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,8 +5,8 @@
   end.join(" ")
 end
 
-def reverse_each_word(sentence1)
-  sentence1.split.collect do |word|
+def reverse_each_word(sentence2)
+  sentence2.split.collect do |word|
     #{word.reverse!}
   end.join(" ")
 end
